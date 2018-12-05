c730e47a0c8afbe9d8f6e54c79b972a36075171f
Gajanan <Gajanan@Gaju>
2018-12-05 12:30:25 +0530

    Here we changes into Robot.java class

--- @AutowiredAnd@Required/src/main/java/com/ld/beans/Robot.java
+++ @AutowiredAnd@Required/src/main/java/com/ld/beans/Robot.java
@@ -8,19 +8,25 @@ public class Robot {
 	private Chip chip;
 	private Sensor sensor;
 
-	@Autowired
-	public Robot(Sensor sensor) {
-		this.sensor = sensor;
-	}
-
-	@Autowired
+	@Autowired(required = false)
 	public Robot(Chip chip) {
 		this.chip = chip;
 	}
 
+	@Autowired(required = false)
+	public Robot(Sensor sensor) {
+		this.sensor = sensor;
+	}
+
+	@Autowired(required=false)
+	public Robot(Chip chip, Sensor sensor) {
+		this.chip = chip;
+		this.sensor = sensor;
+	}
+
 	@Override
 	public String toString() {
-		return "Robot [chip=" + chip + "]";
+		return "Robot [chip=" + chip + ", sensor=" + sensor + "]";
 	}
 
 }
